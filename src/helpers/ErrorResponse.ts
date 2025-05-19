e3032dce25b553edcde76cc275cc28ed42b254d3
vanhoaian <ngohoaian606@gmail.com>
2025-05-19 23:18:44 +0700

    them error response

---
+++ src/helpers/ErrorResponse.ts
@@ -0,0 +1,11 @@
+
+class ErrorResponse extends Error {
+    statusCode: number;
+
+    constructor(statusCode: number, message: string) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = ErrorResponse;
